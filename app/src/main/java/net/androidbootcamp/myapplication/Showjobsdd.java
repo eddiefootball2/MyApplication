6647af72f2e48ac4319c4d24a2e64272207f0712
eddiefootball2 <edward1.whitfield@famu.edu>
2020-03-09 15:35:47 -0400

    Initial commit

---
+++ app/src/main/java/net/androidbootcamp/myapplication/Showjobsdd.java
@@ -0,0 +1,14 @@
+package net.androidbootcamp.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Showjobsdd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_showjobsdd );
+    }
+}
